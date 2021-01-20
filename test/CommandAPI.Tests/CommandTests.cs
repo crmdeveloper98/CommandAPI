14fe798b60234794e60d402308ab06748a7721e9
crm developer <crmdeveloper98@gmail.com>
2021-01-20 07:06:03 +0000

    Unit testing the API

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            // Arrange
+
+            //Act
+            testCommand.Platform = ".NET Core CLI";
+
+            //Assert
+            Assert.Equal(".NET Core CLI", testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            // Arrange
+
+            //Act
+            testCommand.CommandLine = "dotnet run";
+
+            //Assert
+            Assert.Equal("dotnet run", testCommand.CommandLine);
+        }
+
+    }
+}
